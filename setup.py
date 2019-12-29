34fa8fdb84cfb63fd5781ada1f58d19424ce46dd
fwestenberg <47930023+fwestenberg@users.noreply.github.com>
2019-12-29 23:30:40 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'stookalert',
+  packages = ['stookalert'],
+  version = '0.1',
+  license='MIT',
+  description = 'Stookalert package',
+  author = 'fwestenberg',
+  author_email = '',
+  url = 'https://github.com/fwestenberg/stookalert',
+  download_url = 'https://github.com/fwestenberg/stookalert/archive/v_01.tar.gz',
+  keywords = ['Stookalert', 'Home-Assistant'],
+  install_requires=[
+          'json',
+          'requests',
+          'datetime',
+          'logging',
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7'
+  ],
+)
