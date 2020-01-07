11c81d79fc00a658d45367d85d166e6eebeb0363
fwestenberg <47930023+fwestenberg@users.noreply.github.com>
2020-01-07 21:21:05 +0100

    Changed versioning

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'stookalert',
   packages = ['stookalert'],
-  version = '0.1.3',
+  version = '0.1.4',
   license='MIT',
   description = 'Stookalert package',
   author = 'fwestenberg',
   author_email = '',
   url = 'https://github.com/fwestenberg/stookalert',
-  download_url = 'https://github.com/fwestenberg/stookalert/archive/v_012.tar.gz',
+  download_url = 'https://github.com/fwestenberg/stookalert/archive/v_014.tar.gz',
   keywords = ['Stookalert', 'Home-Assistant'],
   install_requires=[
           'requests',
