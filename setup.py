17265df2bba0e8d8ddf8a7aec7676953cb2f9d33
fwestenberg <47930023+fwestenberg@users.noreply.github.com>
2020-01-07 21:08:18 +0100

    Get method for private attributes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'stookalert',
   packages = ['stookalert'],
-  version = '0.1.2',
+  version = '0.1.3',
   license='MIT',
   description = 'Stookalert package',
   author = 'fwestenberg',
